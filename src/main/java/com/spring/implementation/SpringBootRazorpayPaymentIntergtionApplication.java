267b7cfb02175764a956581d1505ff81b5fb17f9
Sakethreddy264 <sakethreddy020604@gmail.com>
2025-04-09 00:02:58 +0530

    Initial commit

---
+++ src/main/java/com/spring/implementation/SpringBootRazorpayPaymentIntergtionApplication.java
@@ -0,0 +1,14 @@
+package com.spring.implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringBootRazorpayPaymentIntergtionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRazorpayPaymentIntergtionApplication.class, args);
+	}
+}
+ 
